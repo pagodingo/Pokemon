10010d689db35d5ca115990197414785f73d6ac4
Daniel Archibald <47116316+PagoDingo@users.noreply.github.com>
2022-02-16 15:08:49 -0500

    Update trending.js

--- netlify/functions/trending/trending.js
+++ netlify/functions/trending/trending.js
@@ -39,10 +39,11 @@ const handler = async (event) => {
                         }) 
                         return trending;
                     }
+      trending = getYoutubeTrending()
       return {
         statusCode: 200,
         //body: getYouTubeTrending().toString(),
-          body: JSON.stringify({test: getYoutubeTrending()})
+          body: JSON.stringify({test: trending})
         // // more keys you can return:
         // headers: { "headerName": "headerValue", ... },
         // isBase64Encoded: true,
