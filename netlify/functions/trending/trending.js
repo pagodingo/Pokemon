7819700d4863d62a92af9d09cc37f21d3dc3e936
Daniel Archibald <47116316+PagoDingo@users.noreply.github.com>
2022-02-17 15:15:52 -0500

    Update trending.js

--- netlify/functions/trending/trending.js
+++ netlify/functions/trending/trending.js
@@ -1,15 +1,11 @@
 const request = require('request');
-async function getYoutubeTrending() {
+function getYoutubeTrending() {
                         //https://www.npmjs.com/package/request
                         let trending = ["foo"]
-                        await request('http://www.youtube.com/trending', function (error, response, body) {
-
-
-
+                        request('http://www.youtube.com/trending', function (error, response, body) {
                             //body = garbled mess of HTML
                             //"watchendpoint" - a recurring pattern I noticed in the HTML that made it easier for me to split it.
                             //splitting by that gave back 100 items, all big chunks of YouTube Meta Data.
-
                             let list = body.split(`"watchEndpoint":{"videoId":"`)
 
                             for (var i = 0; i < list.length - 1; ++i) {
@@ -21,35 +17,27 @@ async function getYoutubeTrending() {
                                 ("title" and "description blah blah"), I used that to grab each video's metaJSON as best I could.
                                 added brackets because they tended to not be there.*/
                                 var videoMeta = "{" + metaChunk.slice(metaChunk.indexOf(`"title"`), metaChunk.indexOf(`,"descriptionSnippet"`)) + "}"
-
                                 /*so, apparently "title" and "description" shows up everywhere on Youtube - who knew?
                                 sometimes what I thought was video meta, wasn't actually video meta.*/
-                                // if 'runs:text' whatever isn't in videoMeta, it's not videoMeta.
+                                // if 'runs:text' isn't in this videoMeta chunk, it's not videoMeta.
                                 if (videoMeta.includes(`{"runs":[{"text"`)) {
-                                    //again, like, brackets were missing.
-                                    //and then I made use of the videoId and like, plugged it back into the metaData
+                                    //again brackets were missing.
+                                    //I made use of the videoId and plugged it back into the metaData
                                     (videoMeta.slice(videoMeta.length - 4, videoMeta.length) != "}}}}") ?
                                         trending.push(Object.assign(JSON.parse(videoMeta + "}"), {"videoId": videoId})) :
                                         trending.push(Object.assign(JSON.parse(videoMeta), {"videoId": videoId}))
                                 }
-                            } // for something that literally should not have worked at all, this code isn't THAT bad. but hey idk.
+                            }
                         }) 
                         return trending;
                     }
     
 const handler = async (event) => {
     try {
-      trending = await getYoutubeTrending()
-        //console.log("foo")
-        //console.log(await getYoutubeTrending())
-       // trending = await getYoutubeTrending()
+      trending = getYoutubeTrending()
       return {
         statusCode: 200,
-        //body: getYouTubeTrending().toString(),
-          body: JSON.stringify({test: trending})
-        // // more keys you can return:
-        // headers: { "headerName": "headerValue", ... },
-        // isBase64Encoded: true,
+        body: {test: trending}
       }
     } catch (error) {
       return { statusCode: 500, body: error.toString() }
