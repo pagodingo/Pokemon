1bac07807d5d07b164c902382765b49a46e8952a
Daniel Archibald <47116316+PagoDingo@users.noreply.github.com>
2022-02-17 14:33:37 -0500

    Update trending.js

--- netlify/functions/trending/trending.js
+++ netlify/functions/trending/trending.js
@@ -1,5 +1,5 @@
 const request = require('request');
-function getYouTubeTrending() {
+function getYoutubeTrending() {
                         //https://www.npmjs.com/package/request
                         let trending = []
                         request('http://www.youtube.com/trending', function (error, response, body) {
