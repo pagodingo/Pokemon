2b75fa8309f88828e7e80be22d644628885011bd
Daniel Archibald <47116316+PagoDingo@users.noreply.github.com>
2022-02-20 14:06:17 -0500

    Update trending.js

--- netlify/functions/trending/trending.js
+++ netlify/functions/trending/trending.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const handler = async (event) => {
     try {
-        return await request('http://www.youtube.com/trending', (function (error, response, body) {
+        return await request('http://www.youtube.com/trending', function (error, response, body) {
           trending = ["foo"]
           list = body.split(`"watchEndpoint":{"videoId":"`)
 
@@ -22,7 +22,7 @@ const handler = async (event) => {
         statusCode: 200,
         body: JSON.stringify({test: trending})
       }
-    }()))
+    })
 
     } catch (error) {
       return { statusCode: 500, body: error.toString() }
